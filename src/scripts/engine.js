98413307e6b24427a016abfa085f348f6e3d0317
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-10-21 13:51:22 -0300

    Criando Assinaturas

---
+++ src/scripts/engine.js
@@ -0,0 +1,75 @@
+// States das variaveis - Contendo todas as variaveis
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById('score-points')
+    },
+    cardsSprites: {
+        avatar: document.getElementById('card-image'),
+        name: document.getElementById('card-name'),
+        type: document.getElementById('card-type'),
+
+    },
+    fieldCards: {
+        player: document.getElementById('player-field-card'),
+        computer: document.getElementById('computer-field-card'),
+    },
+    actions: {
+        button: document.getElementById('next-duel'),
+    }
+};
+
+// Enumeração das cartas -
+
+const playerSides = {
+    player1: "player-field-card",
+    computer: "computer-field-card"
+}
+
+const pathImages = ".src/assets/icons/";
+
+const cardData = [
+{
+    id: 0,
+    name: "Blue Eyes White Dragon",
+    type: "Paper",
+    img: `${pathImages}dragon.png`,
+    winOf:[1],
+    loseOf:[2],
+},
+{
+    id: 1,
+    name: "Dark Magician",
+    type: "Rock",
+    img: `${pathImages}magician.png`,
+    winOf:[2],
+    loseOf:[0]
+},
+{
+    id: 2,
+    name: "Wxodia",
+    type: "Scissors",
+    img: `${pathImages}exodia.png`,
+    winOf:[0],
+    loseOf:[1]
+},
+]
+
+async function drawCards(cardsNumber, fieldSide) {
+    for (let i = 0; i < cardsNumber; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage)
+    }
+}
+
+
+// Função de Start
+function init () {
+    drawCards(5, playerSides.player1)
+    drawCards(5, playerSides.computer)
+}
+
+init ();
